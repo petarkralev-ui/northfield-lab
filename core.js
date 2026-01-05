f868adc0190606181009d6796a597edc0f1ff1c6
petarkralev-ui <petar.kralev@me.com>
2026-01-05 11:05:24 +0000

    Delete core.js

--- core.js
+++
@@ -1,9 +0,0 @@
-function login() {
-  const id = document.getElementById("id").value;
-  const ctx = document.getElementById("ctx").value;
-  const out = document.getElementById("out");
-  if(id==="youshouldntbehere" && ctx==="vxqffwvg"){
-    localStorage.setItem("ceo","true");
-    window.location="terminal.html";
-  } else out.textContent="ACCESS INCOMPLETE";
-}
